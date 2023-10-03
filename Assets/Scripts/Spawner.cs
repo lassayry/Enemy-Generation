b346ba22c6593daf209b60b9d5817468d63715ec
AikoLovely986 <unreallassyary@gmail.com>
2023-10-03 14:02:26 +0300

    GameObject to Enemy

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,15 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private Enemy _prefab;
     [SerializeField] private Vector3 _direction;
 
     public void Spawn()
     {
-        GameObject newEnemy = Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+        Enemy newEnemy = Instantiate(_prefab, transform.position, Quaternion.identity);
         newEnemy.GetComponent<Enemy>().Init(_direction);
     }
 }
